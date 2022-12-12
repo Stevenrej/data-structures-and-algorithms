fe80109dced3cd1fa46680fbd075d5e114868b6d
Stevenrej <steveo732@gmail.com>
2022-12-11 20:30:13 -0600

    class10

---
+++ javascript/whiteboard/class10/index.js
@@ -0,0 +1,13 @@
+let LinkedList = require('./LinkedList.js');
+
+let list = new LinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+
+
+
+
+
+
---
+++ javascript/whiteboard/stack-queue/stackQueue.js
@@ -0,0 +1,86 @@
+'use strict';
+
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(){
+    this.top = null;
+  }
+
+  push(value){
+    let node = new Node(value);
+    node.next = this.top;
+    this.top = node;
+  }
+
+  pop(){
+    if(this.top === null){ return 'null';}
+    let temp = this.top;
+    this.top = this.top.next;
+    temp.next = null;
+    return temp.value;
+  }
+
+  peek(){
+    if(this.top === null){ return 'null';}
+    return this.top.value;
+  }
+
+  isEmpty(){
+    if(this.top === null){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
+
+class Queue {
+  constructor(){
+    this.front = null;
+    this.rear = null;
+  }
+
+  enqueue(value) {
+    let node = new Node(value);
+    if(this.front === null){
+      this.front = node;
+      return this.rear = node;
+    }
+    if(this.rear === this.front){
+      this.rear = node;
+      return this.front.next = this.rear;
+    }
+    this.rear.next = node;
+    this.rear = this.rear.next;
+  }
+
+  dequeue(){
+    if(this.front === null){ return 'null'; }
+    let temp = this.front;
+    this.front = this.front.next;
+    temp.next = null;
+    return temp.value;
+  }
+
+  peek(){
+    if(this.front === null){ return 'null'; }
+    return this.front.value;
+  }
+
+  isEmpty(){
+    if(this.front === null){
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+module.exports = { Stack, Queue };
